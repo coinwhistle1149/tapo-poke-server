f067c67fc29e45f5fdaf1944bae7b02ee83b4eb2
coinwhistle1149 <50396394+coinwhistle1149@users.noreply.github.com>
2026-04-21 11:28:49 +0100

    Set up basic Express server with endpoints

--- server.js
+++ server.js
@@ -1,1 +1,18 @@
+const express = require("express");
+const app = express();
 
+app.get("/", (req, res) => {
+  res.send("Server is running");
+});
+
+app.get("/lights/on", (req, res) => {
+  res.send("ON endpoint hit");
+});
+
+app.get("/lights/off", (req, res) => {
+  res.send("OFF endpoint hit");
+});
+
+app.listen(3000, () => {
+  console.log("Running");
+});
